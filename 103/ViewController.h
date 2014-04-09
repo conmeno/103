eedc91cd3fb935737dee5ae570fc4eb1750088f1
conmeno <conmeno@outlook.com>
2014-04-09 18:46:57 +0700

    test
    test

--- 103/ViewController.h
+++ 103/ViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (nonatomic,retain) IBOutlet UILabel *lable1;
+-(IBAction)buttonclick:(id)sender;
 @end
--- 103/ViewController.m
+++ 103/ViewController.m
@@ -13,12 +13,44 @@
 @end
 
 @implementation ViewController
+@synthesize lable1;
+-(IBAction)buttonclick:(id)sender
+{
+    lable1.text=@"phuong nguyen";
+   
+    NSURL *URL = [NSURL URLWithString:@"http://itmeshop.com/test.txt"];
 
+    NSError *error;
+    NSString *stringFromFileAtURL = [[NSString alloc]
+                                     initWithContentsOfURL:URL
+                                     encoding:NSUTF8StringEncoding
+                                     error:&error];
+    
+    
+    
+    lable1.text=stringFromFileAtURL;
+if(stringFromFileAtURL==nil)
+    lable1.text=@"load ko dc";
+    
+    NSArray *allLines = [stringFromFileAtURL componentsSeparatedByString: @"\n"];
+    
+    NSString *temp=@"";
+    for (int i=0; i<2;i++) {
+        temp =[temp ] [allLines objectAtIndex:2];
+    }
+    NSString *thirdLine = [allLines objectAtIndex:2];
+
+    
+    lable1.text=thirdLine;
+    
+
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
 
 - (void)didReceiveMemoryWarning
 {
